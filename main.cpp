a1de37f7fdfd6c56453ba5aa67dee4e3b5139c25
ivfiev <filjko@ya.ru>
2024-05-12 20:58:48 +0100

    fonts

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <cstdio>
 #include <pico/cyw43_arch.h>
+#include <pico_graphics.hpp>
 #include "pico_explorer.hpp"
 #include "functions.h"
 
 using namespace pimoroni;
 using namespace std;
+
+extern PicoGraphics_PenRGB332 graphics;
 
 int main() {
     stdio_init_all();
@@ -15,23 +18,28 @@ int main() {
     dht_init();
 
     int i = 0;
+    char celsius[20];
+    char humidity[20];
 
     dht_reading dr;
 
     while (true) {
         clear();
 
-        sleep_ms(5000);
+        sleep_ms(2000);
         dht_read(&dr);
-        //printf("%f.0 %f.0\n", dr.temp_celsius, dr.humidity);
 
-        log_msg("%.f Celsius\n%.f Humidity\n\n", dr.temp_celsius, dr.humidity);
-        print_msgs(0);
+        sprintf(celsius, "%.fC", dr.temp_celsius);
+        sprintf(humidity, "%.f%%", dr.humidity);
+
+        graphics.set_font(&hershey::futural);
+        graphics.text(celsius, Point(55, 85), 240);
+        graphics.text(humidity, Point(55, 155), 240);
+
+//        log_msg("%.f Celsius\n%.f Humidity\n\n", dr.temp_celsius, dr.humidity);
+//        print_msgs(0);
 
         redraw();
-
-        //set_led(i & 1);
-
         sleep_ms(1000);
         i++;
     }
